7bdf6b714de26e9cbfc449b760b73c06400c54b6
Riccardo Caroli <beaver82@gmail.com>
2019-12-11 17:31:52 +0100

    chore: header WIP and footer

--- assets/theme.js
+++ assets/theme.js
@@ -4,5 +4,4 @@ import 'xtend-library/src/polyfill.js'
 import 'xtend-library/src/polyfill-old.js'
 import { Xt } from 'xtend-library'
 import 'xtend-library/src/xtend-core.js'
-import 'xtend-library/src/addons/test.js' // example override file: check console.debug
 import gsap from 'gsap'
